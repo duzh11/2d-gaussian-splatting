349e7ad130ad01eedee59e160a822969cd2de350
Du_NUDT <meterodu@163.com>
2024-11-25 21:36:28 +0800

    增加了一个usingmask的参数

--- run_TNT.py
+++ run_TNT.py
@@ -11,13 +11,13 @@ cmd_lis=[]
 #     cmd_lis.append(f'python train.py -s ../../Data/DTU/{scene} -m ../exps/full/DTU/{scene} -r 2 --depth_ratio 1')
 
 # render images,  extract and eval meshes
-cmd_lis.append('python scripts/tnt_eval.py --TNT_data ../../Data/TNT --TNT_GT ../../Data/Official_TNT_dataset --output_path ../exps/full/TNT --skip_metrics')
+cmd_lis.append('python scripts/tnt_eval.py --TNT_data ../../Data/TNT --TNT_GT ../../Data/Official_TNT_dataset --output_path ../exps/wo_depthdist/TNT --skip_training --skip_rendering')
 
 # eval nvs
-for scene in tnt_scenes:
-    cmd_lis.append(f'python metrics.py -m ../exps/full/TNT/{scene} -f train')
-    cmd_lis.append(f'python metrics.py -m ../exps/full/TNT/{scene} -f test')
-    cmd_lis.append(f'python vis_outputs.py -m ../exps/full/TNT/{scene} -f train test')
+# for scene in tnt_scenes:
+#     cmd_lis.append(f'python metrics.py -m ../exps/full/TNT/{scene} -f train')
+#     cmd_lis.append(f'python metrics.py -m ../exps/full/TNT/{scene} -f test')
+#     cmd_lis.append(f'python vis_outputs.py -m ../exps/full/TNT/{scene} -f train test')
 
 # run cmd
 for cmd in cmd_lis:
