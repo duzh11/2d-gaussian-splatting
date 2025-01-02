71ceb8599b043ce62b0c83f8150672007f4ef63d
Du_NUDT <meterodu@163.com>
2025-01-02 16:54:52 +0800

    增加了summary results功能

--- scripts/summary.py
+++ scripts/summary.py
@@ -4,57 +4,57 @@ import json
 import pandas as pd
 import argparse
 
-def main(result_dirs):
-    # Find all results.json files
-    results = glob.glob(os.path.join(result_dirs, '*', 'results.json'))
-
+def summary_results(result_dirs, scene_lis, json_name = 'results_train'):
     # Initialize lists to store the data
     data = []
 
     # Read each JSON file and extract the metrics
-    for result_file in results:
+    for scene in scene_lis:
+        # find the result file
+        if ".csv" not in scene:
+            result_file = os.path.join(result_dirs, scene, f'{json_name}.json')
+        else: 
+            continue
+    
         with open(result_file, 'r') as f:
             result = json.load(f)
         
-        # Extract the experiment name from the file path
-        exp_name = result_file.split('/')[-2]
-        
         # Extract the metrics
-        psnr = result['ours_30000'].get('PSNR', 'N/A')
-        ssim = result['ours_30000'].get('SSIM', 'N/A')
-        lpips = result['ours_30000'].get('LPIPS', 'N/A')
+        result_lis = {'Scene' : scene}
+        result_lis.update(result['ours_30000'])
         
         # Append the data to the list
-        data.append({
-            'Experiment': exp_name,
-            'PSNR': psnr,
-            'SSIM': ssim,
-            'LPIPS': lpips
-        })
+        data.append(result_lis)
+
+    # Calculate average metrics
+    df = pd.DataFrame(data)
+
+    average_lis = {'Scene' : 'average'}
+    for key in result['ours_30000']:
+        average_lis[key] = df[key].mean()
+    
+    data.append(average_lis)
 
     # Create a DataFrame from the collected data
     df = pd.DataFrame(data)
 
     # Sort the DataFrame by experiment name
-    df = df.sort_values('Experiment')
+    df = df.sort_values('Scene')
 
-    # Display the table
-    print(df.to_string(index=False))
-
-    # calculate average PSNR, SSIM, and LPIPS
-    avg_psnr = df['PSNR'].mean()
-    avg_ssim = df['SSIM'].mean()
-    avg_lpips = df['LPIPS'].mean()
-
-    print(f"Average PSNR: {avg_psnr}")
-    print(f"Average SSIM: {avg_ssim}")
-    print(f"Average LPIPS: {avg_lpips}")
-
-    # Optionally, save the table to a CSV file
-    # df.to_csv('results_summary.csv', index=False)
+    # Save the table to a CSV file
+    df.to_csv(f'{result_dirs}/{json_name}_summary.csv', index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process results from JSON files.")
-    parser.add_argument("--model_path", "-m", help="model path")
+    parser.add_argument("--model_path", "-m", help="model path", default="../exps/full/scannetpp-lambdadist-0")
+    parser.add_argument("--scene_lis", nargs="+", type=str, default=[], help="scene list")
     args = parser.parse_args()
-    main(args.model_path)
+    
+    if len(args.scene_lis) == 0:
+        scene_lis = os.listdir(args.model_path)
+    else:
+        scene_lis = args.scene_lis
+
+    for json_name in ['result_mesh_tsdf', 'result_mesh_poisson', 'results_train', 'results_test', \
+                      'results_train_depth', 'results_test_depth', 'results_train_normal', 'results_test_normal']:
+        summary_results(args.model_path, scene_lis, json_name = json_name)
